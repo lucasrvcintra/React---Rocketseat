5fbe54ad4b346f0bd11325296d21c0ab5bcd3c1d
Lucas Cintra <lucasrcintra63@gmail.com>
2024-05-14 15:21:38 -0300

    alterando caminhos e criando componente

--- reactapp/src/Pages/Home/index.jsx
+++ reactapp/src/Pages/Home/index.jsx
@@ -1,9 +1,16 @@
+import "./style.css";
+
+import { Card } from "../../components/Card";
+
 function Home() {
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite um nome..." />
       <button type="button">Adicionar</button>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./Styles/global.css";
-import "./Styles/style.css";
 
-import Home from "./Pages/Home";
+import Home from "./Pages/Home/index";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
