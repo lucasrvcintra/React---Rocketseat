42e93fb10dd838d8e470577a94babe929a7a7e4e
Lucas Cintra <lucasrcintra63@gmail.com>
2024-05-16 12:12:19 -0300

    arquivo final - lista de presenca

--- reactapp/src/Pages/Home/index.jsx
+++ reactapp/src/Pages/Home/index.jsx
@@ -1,18 +1,65 @@
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
 import { Card } from "../../components/Card";
 
-function Home() {
+export function Home() {
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({ name: "", avatar: "" });
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.github.com/users/lucasrvcintra"
+      );
+      const data = await response.json();
+      console.log("DADOS >>>", data);
+
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      });
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite um nome..." />
-      <button type="button">Adicionar</button>
-      <Card />
-      <Card />
-      <Card />
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de Perfil" />
+        </div>
+      </header>
+
+      <input
+        type="text"
+        placeholder="Digite um nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {students.map((student) => (
+        <Card key={student.time} name={student.name} time={student.time} />
+      ))}
     </div>
   );
 }
-
-export default Home;
--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./Styles/global.css";
 
-import Home from "./Pages/Home/index";
+import { Home } from "./Pages/Home/index";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
